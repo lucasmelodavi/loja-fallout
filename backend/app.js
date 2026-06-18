c705b790f58f186f53956caf5e1fd27d709a9247
Davi Lucas Melo <lucasmelodavi425@gmail.com>
2026-06-18 16:30:21 -0300

    aparecer lista de login

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,29 @@ import sqlite3 from "sqlite3";
 
 let db;
 
+async function login(id) {
+  const user = await db.get(
+    "SELECT * FROM usuarios WHERE id = ?", [id]
+);
+return user ;
+}
+
 async function start() {
   db = await open({
-    filename: "./banco.db",
+    filename: "./Banco.db",
     driver: sqlite3.Database,
   });
-  console.log("Banco conectado");
+  console.log("===== LOGIN ======");
+  const user = await login("10012");
 
+  if (user) {
+    console.log("ID:", user.id);
+    console.log("Nome:", user.Sobrevivente);
+    console.log("Tampas:", user["tampas "]);
+    console.log("=================");
+  } else {
+    console.log("Acesso negado");
+  }
 }
 
 start();
