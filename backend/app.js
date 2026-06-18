daf6d65e5548750e9b2009694d0c0dc090fd5381
Davi Lucas Melo <lucasmelodavi425@gmail.com>
2026-06-18 14:50:22 -0300

    conectando banco sql3 do app.js para pasta banco.db

--- backend/app.js
+++ backend/app.js
@@ -1,2 +1,15 @@
-import sqlite from 'sqlite';
-import { open } from 'sqlite';
+import { open } from "sqlite";
+import sqlite3 from "sqlite3";
+
+let db;
+
+async function start() {
+  db = await open({
+    filename: "./banco.db",
+    driver: sqlite3.Database,
+  });
+  console.log("Banco conectado");
+
+}
+
+start();
