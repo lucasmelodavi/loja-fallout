d67f37a484e7dfb300b53d36e832df7c2ed9d310
Davi Lucas Melo <lucasmelodavi425@gmail.com>
2026-06-22 16:03:03 -0300

    mais ajustes no serve.js

--- backend/server.js
+++ backend/server.js
@@ -9,20 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/login", loginRoutes);
-app.use("/itens", itensRoutes)
-
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
-  if (req.method === "OPTIONS") return res.sendStatus(200);
-
-  next();
-});
-
-app.use("/login", loginRoutes);
+app.use("/itens", itensRoutes);
 
 app.listen(3000, () => {
   console.log("Servidor rodando na porta 3000");
